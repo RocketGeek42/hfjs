b88130d6cbd894dcdce686173e96defd7e2583bb
Jen York <jen@yorkitconsulting.com>
2019-02-13 11:38:11 -0600

    Add files via upload

---
+++ Ch4/most_cost_effective.js
@@ -0,0 +1,15 @@
+function getMostCostEffectiveSolution(scores, costs, highScore) {
+    var cost = 100;
+    var index;
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            if (cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return index;
+}
+var mostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
---
+++ Ch4/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+var hasBubbleGum = [false, false, false, true];
